275f65ea82611dba796516fe11c38757e81f1ffe
Likhith750 <doddilikhithkumar750@gmail.com>
2024-09-19 16:20:39 +0530

    Update config.js

--- dist/config/config.js
+++ dist/config/config.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.default = {
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://0.0.0.0:27017/joeys_db',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/joeys_db',
     JWT_SECRET: process.env.JWT_SECRET || '',
     PORT: process.env.PORT || 5000,
 };
