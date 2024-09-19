a45dd38583e3383fa869a3b87eb85028c0e3ecdb
Likhith750 <doddilikhithkumar750@gmail.com>
2024-09-19 16:22:42 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://0.0.0.0:27017/joeys_db',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/joeys_db',
     JWT_SECRET: process.env.JWT_SECRET || '',
     PORT: process.env.PORT || 5000,
 };
